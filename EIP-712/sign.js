19aa1afcb7609aae23403ea43b1de40e21ce5598
Rémy Roy <remyroy@remyroy.com>
2021-01-21 12:36:29 -0500

    Update usage example with modern JS and ethers.js

--- EIP-712/sign.js
+++ EIP-712/sign.js
@@ -1,118 +1,65 @@
-function parseSignature(signature) {
-  var r = signature.substring(0, 64);
-  var s = signature.substring(64, 128);
-  var v = signature.substring(128, 130);
-
-  return {
-      r: "0x" + r,
-      s: "0x" + s,
-      v: parseInt(v, 16)
-  }
+if (typeof window.ethereum !== 'undefined') {
+  console.log('MetaMask is installed!');
 }
 
+const connectWalletButton = document.querySelector('#connectWallet');
 
-window.onload = function (e) {
-  var res = document.getElementById("response");
-  res.style.display = "none";
+connectWalletButton.addEventListener('click', () => {
+  //Will Start the metamask extension
+  ethereum.request({ method: 'eth_requestAccounts' });
+});
 
-  // force the user to unlock their MetaMask
-  if (web3.eth.accounts[0] == null) {
-    alert("Please unlock MetaMask first");
-    // Trigger login request with MetaMask
-    web3.currentProvider.enable().catch(alert)
+async function sign()
+{
+  const accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+
+  if (accounts[0] == null) {
+    return;
   }
 
-  var signBtn = document.getElementById("signBtn");
-  signBtn.onclick = function(e) {
-    if (web3.eth.accounts[0] == null) {
-      return;
-    }
-
-    const domain = [
-      { name: "name", type: "string" },
-      { name: "version", type: "string" },
-      { name: "chainId", type: "uint256" },
-      { name: "verifyingContract", type: "address" }
-    ];
-
-    const unit = [
-      { name: 'actionType', type: 'string' },
-      { name: 'timestamp', type: 'uint256' },
-      { name: 'authorizer', type: 'string' }
-    ];
-
-//    const chainId = 42;
-    const chainId = 5; // goerli testnet
-
-    const domainData = {
-      name: "VerifierApp101",
-      version: "1",
-      chainId: chainId,
-      verifyingContract: "0x8c1eD7e19abAa9f23c476dA86Dc1577F1Ef401f5"
-    };
-
-    var message = {
-        actionType: 'Action7440',
-        timestamp: 1570112162,
-        authorizer: 'auth239430'
-    };  // order of the fields is is important. Should correspond to the tuple type expected on the contract.
-
-    const data = JSON.stringify({
-      types: {
-        EIP712Domain: domain,
-        Unit: unit
-      },
-      domain: domainData,
-      primaryType: "Unit",
-      message: message
-    });
-
-    const signer = web3.toChecksumAddress(web3.eth.accounts[0]);
-
-    web3.currentProvider.sendAsync(
-      {
-        method: "eth_signTypedData_v3",
-        params: [signer, data],
-        from: signer
-      },
-      function(err, result) {
-        if (err || result.error) {
-          return console.error(result);
-        }
-
-        const signature = parseSignature(result.result.substring(2));
-
-        res.style.display = "block";
-        res.value = "SigR: "+signature.r+"\nSigS: "+signature.s+"\nSigV: "+signature.v+"\nSigner: "+signer;
-//        res.value += "\n\nEIP-712 Message sent:\n"+data;
-
-        var xhr = new XMLHttpRequest();
-        var url = 'http://localhost:6635/flat'
-        xhr.open("POST", url, true);
-        xhr.setRequestHeader("Content-Type", "application/json");
-
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-                var json = JSON.parse(xhr.responseText);
-                window.alert('Submitted succesfully. Check tornado backend logs and console logs.')
-                console.log(json)
-            }
-        };
-
-        var data = JSON.stringify({
-            command: 'submitProof',
-            // replace the following with the deployed EIP712FlatStruct.sol contract address
-            contractAddress: '0x8e12f01dae5fe7f1122dc42f2cb084f2f9e8aa03',
-            messageObject: message,
-            sigR: signature.r,
-            sigS: signature.s,
-            sigV: signature.v,
-            signer: signer
-
-        });
-
-        xhr.send(data);
-      }
-    );
+  const domain = {
+    name: 'Ether Mail',
+    version: '1',
+    chainId: 1,
+    verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC'
   };
+
+  // The named list of all type definitions
+  const types = {
+      Person: [
+          { name: 'name', type: 'string' },
+          { name: 'wallet', type: 'address' }
+      ],
+      Mail: [
+          { name: 'from', type: 'Person' },
+          { name: 'to', type: 'Person' },
+          { name: 'contents', type: 'string' }
+      ]
+  };
+
+  // The data to sign
+  const value = {
+      from: {
+          name: 'Cow',
+          wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826'
+      },
+      to: {
+          name: 'Bob',
+          wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB'
+      },
+      contents: 'Hello, Bob!'
+  };
+
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+
+  // Might need to replace _signTypedData with signTypedData as mentionned on https://docs.ethers.io/v5/api/signer/#Signer-signTypedData
+  const signature = await signer._signTypedData(domain, types, value);
+  console.log(signature);
 }
+
+const signBtn = document.querySelector('#signBtn');
+
+signBtn.addEventListener('click', () => {
+  sign();
+});
